c57fe9f721428f1dda771ee2e34a75605b0ecb2a
Andrew Lechner <alechner@gmail.com>
2022-12-09 11:17:25 -0800

    Adding tests for main

--- app/src/test/java/riot/gameoflife/AppTest.java
+++ app/src/test/java/riot/gameoflife/AppTest.java
@@ -1,7 +1,89 @@
 package riot.gameoflife;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class AppTest {
     
-    // TODO: Should add some tests around main app as well
-    
+    @Test
+    public void appEmptyInput() {
+        String input = String.format("");
+        ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
+        System.setIn(bais);
+
+        String expected = "#Life 1.06" + System.lineSeparator();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        App.main(null);
+
+        String actual = baos.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void appValidSingleLine() {
+        String input = String.format("(0,0)");
+        ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
+        System.setIn(bais);
+
+        String expected = "#Life 1.06" + System.lineSeparator();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        App.main(null);
+
+        String actual = baos.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void appValidSingleLineWhitespaceNegative() {
+        String input = String.format(" ( -987432 , -478324 ) ");
+        ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
+        System.setIn(bais);
+
+        String expected = "#Life 1.06" + System.lineSeparator();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        App.main(null);
+
+        String actual = baos.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void appValidEvolution() {
+        String input = String.format("(0, 0)" + System.lineSeparator() +
+        "(0, -1)" + System.lineSeparator() +
+        "(0, 1)");
+        ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
+        System.setIn(bais);
+
+        String expected = "#Life 1.06" + System.lineSeparator() +
+        "0 -1" + System.lineSeparator() +
+        "0 0" + System.lineSeparator() +
+        "0 1" + System.lineSeparator();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        App.main(null);
+
+        String actual = baos.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    // TODO: main calls System.Exit(1) on invalid input; need to research testing that
 }
